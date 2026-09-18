e70a6c015576cca22f217de593c33a149d50b3e6
TECMDUOCUC <to.cornejom@duocuc.cl>
2026-09-18 01:08:59 -0300

    Added application logo, worked on the UI slightly

--- Test/app/src/main/java/com/example/juntavecinos/MainActivity.kt
+++ Test/app/src/main/java/com/example/juntavecinos/MainActivity.kt
@@ -4,44 +4,106 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.juntavecinos.ui.theme.JuntaVecinosTheme
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.juntavecinos.ui.theme.*
+
+// ...
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JuntaVecinosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            HomeScreen()
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+data class Message(val author: String, val body: String)
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    JuntaVecinosTheme {
-        Greeting("Android")
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .safeDrawingPadding()
+            .background(Blue3)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                .fillMaxWidth()
+                .height(55.dp)
+                .background(Blue1),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Junta Vecinos App")
+            }
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .height(15.dp)
+                .background(Blue2))
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Text("Seleccione su rol.")
+                Spacer(modifier = Modifier.height(50.dp))
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
+                    Button(onClick = {}){
+                        Text(
+                            "¿Eres un miembro\nde la directiva?",
+                            textAlign = TextAlign.Center
+                        )
+                    }
+
+                    Button(onClick = {}){
+                        Text(
+                            "¿Eres un vecino?",
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+            }
+
+        }
     }
 }
